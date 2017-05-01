ee836863fa222ca9c867c3baa8266c31f44778aa
Alvaro Miranda Aguilera <kikitux@gmail.com>
2017-05-01 15:22:18 +0200

    server.sh - initial

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
 echo "running server provisioning"
+
+which redis-server 2>/dev/nul || {
+  export DEBIAN_FRONTEND=noninteractive
+  apt-get update
+  apt-get install -y redis-server
+  sed -i 's/bind.*/bind 0.0.0.0/' /etc/redis/redis.conf
+  update-rc.d redis-server defaults
+}
+/etc/init.d/redis-server restart 2>/dev/null
