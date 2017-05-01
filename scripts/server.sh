e82a341cf7f9b26664d4cdedc52a95c2fedf3a44
Niels Abels <n.abels@coolblue.nl>
2017-05-01 15:21:30 +0200

    Installs redis-server on server.

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
 echo "running server provisioning"
+
+# Got this from https://gist.github.com/helloIAmPau/8149357#file-vagrantfile-L10-L15
+
+sudo apt-get install -y redis-server
+sudo cp /etc/redis/redis.conf /etc/redis/redis.conf.old
+sudo cat /etc/redis/redis.conf.old | grep -v bind > /etc/redis/redis.conf
+echo "bind 0.0.0.0" >> /etc/redis/redis.conf
+sudo update-rc.d redis-server defaults
+sudo /etc/init.d/redis-server start
