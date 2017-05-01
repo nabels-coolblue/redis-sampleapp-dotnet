8145c350c002e7596ca942522e0ee46903381f47
Niels Abels <n.abels@coolblue.nl>
2017-05-01 17:57:38 +0200

    Adds dotnet core app to set value in Redis.

---
+++ netcore-redis-app/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ServiceStack.Redis;
+
+namespace netcore_redis_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var manager = new RedisManagerPool("192.168.56.11:6379");
+            using (var client = manager.GetClient())
+            {
+                client.Set("date", DateTime.UtcNow);
+            }
+        }
+    }
+}
