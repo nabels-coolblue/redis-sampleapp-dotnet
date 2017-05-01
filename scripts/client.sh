35d6d60e87ed3c21c6101db1c06a461b7bd86edb
Alvaro Miranda Aguilera <kikitux@users.noreply.github.com>
2017-05-01 15:32:56 +0200

    Update client.sh

--- scripts/client.sh
+++ scripts/client.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
 echo "running client provisioning"
+
+which redis-cli 2>/dev/null || {
+  export DEBIAN_FRONTEND=noninteractive
+  apt-get update
+  apt-get install -y redis-tools
+}
