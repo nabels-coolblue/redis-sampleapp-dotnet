b0031734034bc8b288f874f0a133499909584a6c
Niels Abels <n.abels@coolblue.nl>
2017-05-08 19:49:31 +0200

    Improves performance.

--- netcore-redis-app/runme.sh
+++ netcore-redis-app/runme.sh
@@ -4,8 +4,9 @@ echo "running dotnet core app"
 if [ -d /vagrant/netcore-redis-app/ ]; then
         pushd /vagrant/netcore-redis-app/
         dotnet restore
-		dotnet build
-        time dotnet run
+        dotnet build
+        pushd bin/Debug/netcoreapp1.1
+        time dotnet netcore-redis-app.dll
 else
         echo "ERROR: .net core app could not be found"
 fi
